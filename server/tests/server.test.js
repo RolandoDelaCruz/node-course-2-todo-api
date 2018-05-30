8ce0dabfb67deefb68a5f58b90bd6a26e8daf647
Rolando <rolando.delacruz@gmail.com>
2018-05-30 23:13:45 +0800

    Init commit Add test cases for GET /todos:ids

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,14 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/Todo');
+const {ObjectID} = require('mongodb');
+
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'Fist test Todo'
 },{
+    _id: new ObjectID(),
     text: 'Second test Todo'
 }];
 
@@ -68,5 +72,35 @@ describe('GET /todos', () =>{
             })
             .end(done);
     });
+});
+
+describe('GET /todos/:id',()=>{
+    it('should return todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) =>{
+                expect(res.body.todos.text).toBe(todos[0].text)
+            })
+            .end(done)
+    });
+
+    it('should return 404 if todo not found', (done) =>{
+        var hexId = new ObjectID().toHexString();
+       request(app)
+        .get(`/todos/${hexId}}`)
+        .expect(404)        
+        .end(done)
+    })
+
+    it('should return 404 for non-object ids',(done)=>{
+        request(app)
+            .get('/todos/12345')
+            .expect(404)
+            .end(done)
+    })
+
+
 })
+
 
