d8a674c9e3d02276eba7933450462663689d03c5
Rolando <rolando.delacruz@gmail.com>
2018-05-31 23:40:26 +0800

    Patch todo api

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
-var express = require('express');
-var bodyParser = require('body-parser');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -69,7 +70,33 @@ app.delete('/todos/:id',(req,res) =>{
     }, (e) =>{
         res.status(400).send(e)
     });
+});
+
+
+app.patch('/todos/:id',(req,res) =>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todos) =>{
+        if(!todos){
+            return res.status(404).send();
+        }
+        res.send({todos});
+    }).catch((e) => res.status(400).send())
+
 })
+
 
 app.listen(port, ()=>{
      console.log(`Started up at port ${port}`);
